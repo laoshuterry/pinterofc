25d4f687c06a5d751eb5ba46843e6f044386697a
Rain Tian <dictatorterry@gmail.com>
2016-05-26 12:21:39 +0800

    理解了多维指针和多维数组之间的概念和联系

--- Chapter04.c
+++ Chapter04.c
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
 		"Hamlet",
 		"Gulliver's Travels"
 	};
-	char titleArray[][30]={
+	char titleArray[7][30]={
 		"A Tale of Two Cities",
 		"Wuthering Heights",
 		"Don Quixote",
@@ -20,10 +20,26 @@ int main(int argc, char const *argv[])
 		"Gulliver's Travels"
 	};
 	//对多维数组仅可省略最左侧一维的的大小。
-	char **title_ptr  = titles;
-	// char **title_pti  = *(titleArray+0);
+	//指针类型与接引操作是不同的
+	//二维指针对应的是指针数组，二维数组对应的是数组指针
+
+	//二维数组中
+	//int matrix[5][5],matrix代表首元素matrix[0]地址，而首元素matrix[0]是个数组，
+	//意味着matrix[0]代表内层数组首元素matrix[0][0]的地址，所以则有
+	//matrix=&matrix[0],matrix[0]=&matrix[0][0],进行解引则有
+	//*matrix=matrix[0]=&matrix[0][0]
+	//**matrix=*matrix[0]=matrix[0][0]
+	
+	//matrix的本质是一个5个元素数组的**数组**指针，但其代表的是整个二维数组，也就是其大小为25*int
+	//matrix[0]的本质是一个5个元素数组**char**指针，但其代表的是内层一维数组，也就是起大小为5*int
+	
+	
+	char **title_ptr; 
+	char (*title_pti)[30];
+	title_ptr = titles;
+  	title_pti = titleArray;
 
 	printf("%s and %c\n", titles[0], **title_ptr);
-	// printf("%s and %c\n", titleArray[0], **title_pti);
+	printf("%s and %c\n", titleArray[0], **title_pti);
 	return 0;
 }
