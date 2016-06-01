50a6403258465a3cf65c94cac2cd12b9efaf5ec4
Y.Tian <dictatorterry@msn.com>
2016-06-01 20:45:49 +0800

    添加了字符串比较和复制操作的样例代码

--- strMem.c
+++ strMem.c
@@ -3,17 +3,46 @@
  *
  *       Filename:  stringMem.c
  *
- *    Description:  
+ *    Description:  Example code to show how the string variables located.
  *
  *        Version:  1.0
  *        Created:  2016年05月31日 17时49分28秒
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
- *   Organization:  
+ *         Author:  Y.Tian 
  *
  * =====================================================================================
  */
-#include <stdlib.h>
+#include "TYheader.h"
+ /*全局变量，位于全局内存*/
+char	*globalHeader = "Hello World!";
+char 	globalArray[] = "Hello World!";
 
+void displayString()
+{
+	/*静态变量，位于全局内存*/
+	static char *staticHeader = "Hello World!";
+	static char staticArray[] = "Hello World!";
+	/*局部变量，位于自动内存*/
+	char *localHeader         = "Hello World!";
+	char localArray[]         = "Hello World!";
+	/*动态变量，位于动态内存（堆）*/
+	char *heapHeader              = (char *)malloc(strlen("Hello World!")+1);
+	strcpy(heapHeader,"Hello World!");
+
+	printf("staticHeader string is: %s and the address is:%p\n", staticHeader , staticHeader);
+	printf("staticArray string is: %s and the address is:%p\n", staticArray , staticArray);
+	printf("localHeader string is: %s and the address is:%p\n", localHeader, localHeader );
+	printf("localArray string is: %s and the address is:%p\n", localArray, localArray );
+	printf("heapHeader string is: %s and the address is:%p\n", heapHeader, heapHeader );
+}
+
+int main(int argc, char const *argv[])
+{
+	displayString();
+	printf("globalHeader string is: %s and the address is:%p\n", globalHeader , globalHeader);
+	printf("globalArray string is: %s and the address is:%p\n", globalArray , globalArray);
+
+	return 0;
+}
--- strStdOper.c
+++ strStdOper.c
@@ -3,17 +3,49 @@
  *
  *       Filename:  stdStringOper.c
  *
- *    Description: 
+ *    Description:  Example code to show how the standard string library worked
  *
  *        Version:  1.0
  *        Created:  2016年05月31日 17时50分17秒
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
- *   Organization:  
+ *         Author:  Y.Tian
  *
  * =====================================================================================
  */
-#include <stdlib.h>
+#include "TYheader.h"
+int main(int argc, char const *argv[])
+{
+	/*字符串比较函数*/
+	char cmd[20];
+	printf("Please enter the quit command: ");
+	scanf("%s", cmd);
+	if (!strcmp(cmd, "Quit"))
+		printf("Command is Right!\n");
+	else
+		printf("Command is Wrong!\n");
 
+	/*字符串复制函数*/
+	int count=0;
+	char singleWord[10];
+	char *wordList[5];
+
+	while( count < 4 )
+	{	
+		memset(singleWord,0,10);
+		printf("Please enter a word: ");
+		scanf("%s", singleWord);
+		wordList[count] = (char *)malloc(strlen(singleWord)+1);
+		strcpy(wordList[count], singleWord);
+		++count;
+	}
+
+	wordList[4]=wordList[3];
+
+	for ( count = 0; count < 5; ++count )
+		printf("The word in the list is: %s\n", wordList[count]);
+
+
+	return 0;
+}
